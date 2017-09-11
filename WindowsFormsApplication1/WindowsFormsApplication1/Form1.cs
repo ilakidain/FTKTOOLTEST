ed83f6f50d7607ede4a9efbeb5a5782b79e85348
ilakidain <ilakidain@ILAKIDAIN>
2017-09-11 16:46:37 +0200

    Inicio de proyecto de prueba

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WindowsFormsApplication1.Properties;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		#region Propiedades
+		#endregion
+
+		#region Contructores
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		#endregion
+
+		#region Eventos
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.ApplyResources();
+		}
+		#endregion
+
+		#region Metodos
+		private void ApplyResources()
+		{
+			//this.Text= Resources.UI_LOQUESEA;
+		}
+
+		#endregion
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Test");
+		}
+	}
+}
+
